c9eaf9c5d70978862ad7113318f0793027657211
abhikdeb <debabhik@gmail.com>
2020-11-11 16:26:44 -0500

    Added Elevation API and Config file

--- EleNa/src/app/data_model/data_model.py
+++ EleNa/src/app/data_model/data_model.py
@@ -1,17 +1,16 @@
-import os
 import osmnx as ox
 import pickle as pickle
-import glob
 import json
 
 
 class DataModel:
 
     def __init__(self, city_name='Amherst, MA'):
-
+        self.config = {}
+        self.load_config('../config.json')
         self.graphs_location = 'stored_locations/'
         self.load_locations_metadata()
-        
+
         if city_name in self.loaded_graphs:
             self.load_graph_from_path(self.loaded_graphs[city_name])
             print('Loaded graph for {} from dump'.format(city_name))
@@ -23,13 +22,22 @@ class DataModel:
             self.update_locations_metadata()
             print('Loaded graph for {} and added to dump'.format(city_name))
 
+    def load_config(self, loc):
+        try:
+            cfg_file = open(loc, "r")
+            self.config = cfg_file.load(cfg_file)
+        except OSError as err:
+            print("Config File Read Error: {0}".format(err))
+        else:
+            cfg_file.close()
+
     def load_locations_metadata(self):
         with open(self.graphs_location + 'metadata.json') as f:
             self.loaded_graphs = json.load(f)
 
     def update_locations_metadata(self):
-        with open(self.graphs_location + 'metadata.json', "w") as outfile:  
-            json.dump(self.loaded_graphs, outfile, indent = 4) 
+        with open(self.graphs_location + 'metadata.json', "w") as outfile:
+            json.dump(self.loaded_graphs, outfile, indent=4)
 
     def plot_graph(self):
         ox.plot_graph(self.G)
@@ -54,5 +62,12 @@ class DataModel:
         return self.G
 
     def add_elevation_to_graph(self):
-        # Todo: Abhik D.
+        if len(self.config.keys()) == 0 or 'api_key' not in self.config['google_api']:
+            print("Config File Error")
+            raise ValueError
+        ox.elevation.add_node_elevations(self.G, self.config['google_api']['api_key'],
+                                         max_locations_per_batch=self.config['google_api']['elevation_api'][
+                                             'batch_size'],
+                                         precision=self.config['google_api']['elevation_api']['precision'])
+        ox.elevation.add_edge_grades(self.G, add_absolute=self.config['google_api']['elevation_api']['add_absolute'])
         pass
